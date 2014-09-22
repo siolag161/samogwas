890ac419844358b00bff309656ca355053aa9b42
reviewerchris <christine.sinoquet@univ-nantes.fr>
2014-09-22 10:23:25 +0200

    Update dissimilarity.hpp

--- core/include/distance/dissimilarity.hpp
+++ core/include/distance/dissimilarity.hpp
@@ -1,18 +1,17 @@
 /****************************************************************************************
  * File: dissimilarity.hpp
  * Description: Grouping all the dissimilarity methods, including:
- * -----------  + Mutual Information based Dissimilarity
- * -----------  + Euclide Distance
- * @author: Duc-Thanh Phan Duc-Thanh Phan siolag161 (thanh.phan@outlook.com), under the supervision of Christine Sinoquet // CS affiliation
- * under the supervision of Christine Sinoquet
+ * -----------  + Mutual Information based Dissimilarity (implemented)
+ * -----------  + Euclidian Distance (to be implemented)
+ * @author: Duc-Thanh Phan Duc-Thanh Phan siolag161 (thanh.phan@outlook.com), under the supervision of Christine Sinoquet
  * @date: 30/07/2014
 
  ***************************************************************************************/
 #ifndef SAMOGWAS_DISSIMILARITY_HPP
 #define SAMOGWAS_DISSIMILARITY_HPP
 
-#include "information_dissimilarity.hpp"
-// CS Should information_dissimilarity.hpp not be renamed in dissimilarity.hpp?
+#include "information_dissimilarity.hpp" //@todo: mutual_information_dissimilarity.hpp
+//#include "euclidian_dissimilarity.hpp"
 
 /****************************************************************************************/
 #endif // SAMOGWAS_DISSIMILARITY_HPP
