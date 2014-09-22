284d3a313a552e5d7699d319699aaaa29002c93e
reviewerchris <christine.sinoquet@univ-nantes.fr>
2014-09-22 10:25:20 +0200

    Update similarity.hpp

--- core/include/distance/similarity.hpp
+++ core/include/distance/similarity.hpp
@@ -1,6 +1,8 @@
 /****************************************************************************************
  * File: similarity.hpp
- * Description: 
+ * Description: Grouping all the similarity methods, including:
+ * -----------  + Mutual Information based similarity (implemented)
+ * -----------  + Euclidian Similarity (to be implemented)
  * @author: Duc-Thanh Phan siolag161 (thanh.phan@outlook.com), under the supervision of Christine Sinoquet
  * @date: 30/07/2014
 
@@ -9,7 +11,8 @@
 #define SAMOGWAS_SIMILARITY_HPP
 
 
-#include "information_similarity.hpp"
+#include "information_similarity.hpp" // @todo: change the name to mutual_information_similarity.hpp
+// #include "euclidian_similarity.hpp"
 
 /****************************************************************************************/
 #endif // SAMOGWAS_SIMILARITY_HPP
