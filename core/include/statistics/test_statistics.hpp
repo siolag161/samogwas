26331da0ebc8d397d0c0e0d7bb91ecb256515b12
siolag <thanh.phan@outlook.com>
2014-09-26 12:38:29 +0200

    Update test_statistics.hpp

--- core/include/statistics/test_statistics.hpp
+++ core/include/statistics/test_statistics.hpp
@@ -1,15 +1,16 @@
 /****************************************************************************************
- * File: statistics.hpp
- * Description: This provides the common interface for all the available templated association tests
- * -----------  Fisher exact test, ChiSquared (with and without correction), G2 (with and without correction)
- * @author: Phan Duc Thanh (duc-thanh.phan@univ-nantes.fr) - Under supervision of Christine Sinoquet (christine.sinoquet@univ-nantes.fr)
+ * File: test_statistics.hpp
+ * Description: This module provides the common interface for all the available templated association tests:
+ * -----------  Fisher exact test, ChiSquared (with and without Yates' correction), 
+ * -----------  G2 (with and without Yates' correction).
+ * @author: Duc-Thanh Phan siolag161 (thanh.phan@outlook.com), under the supervision of Christine Sinoquet
  * @date: 22/06/2014
 
  ***************************************************************************************/
-#ifndef STATS_STATISTICS_HPP
-#define STATS_STATISTICS_HPP
+#ifndef STATS_TEST_STATISTICS_HPP
+#define STATS_TEST_VecTypeypeHPP
 
-#include <vector>
+#include <VecTypeypeor>
 #include "g2.hpp"
 #include "fisher.hpp"
 #include "chisq.hpp"
@@ -27,91 +28,93 @@ enum TestType { G2 = 0, G2_YATES, CHISQ, CHISQ_YATES, FISHER };
 
 template< int Test >
 struct StatisticTest {
+  
   static const int test = (Test == G2) ? G2 :
                           (Test == G2_YATES) ? G2_YATES :
                           (Test == CHISQ) ? CHISQ :
                           (Test == CHISQ_YATES) ? CHISQ_YATES : FISHER;
 
-  template<class ContingencyTableT>
-  double operator()( ContingencyTableT& mat ) const;
+  template<class ContingencyTableType>
+  double operator()( ContingencyTableType& mat ) const;
 
   /** @param
    *  @param 
    * requires that data.size() == which.size()
    */ 
-  template<class VecT>
-  double operator()( const VecT& geno,
-                     const VecT& pheno,
+  template<class VecType>
+  double operator()( const VecType& geno,
+                     const VecType& pheno,
                      const int cardGenotype,
                      const int cardPhenotype ) const {
-    std::vector< std::vector<double> > contingencyTab( cardGenotype, std::vector<double>(cardPhenotype, 0.0) );
+    std::vector< std::vector<double> > contingencyTab( cardGenotype, 
+                                                       std::vector<double>(cardPhenotype, 0.0) );
     for (int i = 0; i < geno.size(); ++i) {
       int row = geno[i];
       int col = pheno[i];
       ++contingencyTab[row][col];
     };
-    return pval( contingencyTab, Int2Type<test>() );    
+    return p_value( contingencyTab, Int2Type<test>() );    
   }
   
  private:  
-  template<class ContingencyTableT>
-  double pval( ContingencyTableT& mat, Int2Type<G2> ) const;
+  template<class ContingencyTableType>
+  double p_value( ContingencyTableType& mat, Int2Type<G2> ) const;
 
-  template<class ContingencyTableT>
-  double pval( ContingencyTableT& mat, Int2Type<G2_YATES> ) const;
+  template<class ContingencyTableType>
+  double p_value( ContingencyTableType& mat, Int2Type<G2_YATES> ) const;
   
-  template<class ContingencyTableT>
-  double pval( ContingencyTableT& mat, Int2Type<CHISQ> ) const;
+  template<class ContingencyTableType>
+  double p_value( ContingencyTableType& mat, Int2Type<CHISQ> ) const;
 
-  template<class ContingencyTableT>
-  double pval( ContingencyTableT& mat, Int2Type<CHISQ_YATES> ) const;
+  template<class ContingencyTableType>
+  double p_value( ContingencyTableType& mat, Int2Type<CHISQ_YATES> ) const;
   
-  template<class ContingencyTableT>
-  double pval( ContingencyTableT& mat, Int2Type<FISHER> ) const;
+  template<class ContingencyTableType>
+  double p_value( ContingencyTableType& mat, Int2Type<FISHER> ) const;
 
 };
 
-} // namespace stats ends here. stats
+} // namespace stats ends here. 
 
 /****************************** IMLEMENTATION BELOW THIS POINT **************************/
 namespace stats
 {
 
 template< int Test >
-template<class ContingencyTableT>
-double StatisticTest<Test>::pval( ContingencyTableT& mat, Int2Type<G2> ) const {
+template<class ContingencyTableType>
+double StatisticTest<Test>::p_value( ContingencyTableType& mat, Int2Type<G2> ) const {
   TestG2 g2;
   return g2.gtest(mat, false);
 }
 
 
 template< int Test >
-template<class ContingencyTableT>
-double StatisticTest<Test>::pval( ContingencyTableT& mat, Int2Type<G2_YATES> ) const {
+template<class ContingencyTableType>
+double StatisticTest<Test>::p_value( ContingencyTableType& mat, Int2Type<G2_YATES> ) const {
   TestG2 g2;
   return g2.gtest(mat, true);
 }
 
 template< int Test >
-template<class ContingencyTableT>
-double StatisticTest<Test>::pval( ContingencyTableT& mat, Int2Type<CHISQ> ) const {
+template<class ContingencyTableType>
+double StatisticTest<Test>::p_value( ContingencyTableType& mat, Int2Type<CHISQ> ) const {
   TestChiSquared chisq;
   return chisq.chisqTest(mat, false);
 }
 
 
 template< int Test >
-template<class ContingencyTableT>
-double StatisticTest<Test>::pval( ContingencyTableT& mat, Int2Type<CHISQ_YATES> ) const {
+template<class ContingencyTableType>
+double StatisticTest<Test>::p_value( ContingencyTableType& mat, Int2Type<CHISQ_YATES> ) const {
   TestChiSquared chisq;
   return chisq.chisqTest(mat, true);
 }
 
 template< int Test >
-template<class ContingencyTableT>
-double StatisticTest<Test>::pval( ContingencyTableT& mat, Int2Type<FISHER> ) const {
+template<class ContingencyTableType>
+double StatisticTest<Test>::p_value( ContingencyTableType& mat, Int2Type<FISHER> ) const {
   TestFisher fisher;
-  return fisher.gtest(mat);
+  return fisher.fisherTest(mat);
 }
 
 
